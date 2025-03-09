fad77bef76f68c86dddfacdd2b354bdf2caac860
HEMANTH <163557351+hemanth8705@users.noreply.github.com>
2025-03-09 16:06:20 +0530

    last commit bro

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -2,12 +2,11 @@ import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
+
 export const axiosInstance = axios.create({
-    baseURL: BASE_URL, // Use the BASE_URL constant here
+    baseURL: import.meta.env.VITE_REACT_APP_API_URL,
     withCredentials: true,
-    timeout: 120000,
     headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
+        'Content-Type': 'application/json'
     }
 });
