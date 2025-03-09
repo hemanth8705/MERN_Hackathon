1bc7fb7d44dd4dbd28b57fcfd4a6f65adab60a1b
HEMANTH <163557351+hemanth8705@users.noreply.github.com>
2025-03-09 15:54:41 +0530

    cleared errors in axios instance -- checked in local

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
-const axiosInstance = axios.create({
-    baseURL: process.env.VITE_REACT_APP_API_URL,
+export const axiosInstance = axios.create({
+    baseURL: BASE_URL, // Use the BASE_URL constant here
     withCredentials: true,
     timeout: 120000,
     headers: {
